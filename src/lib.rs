46de2f435d4ab3e78ee750d9891f4e70b5c7a169
Roman Chumak <p4ymak@gmail.com>
2020-12-13 16:14:51 +0300

    Triangle

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,16 @@
+struct Point {
+    x: f32,
+    y: f32,
+    z: f32,
+}
 struct Triangle {
-    a: Point3<f32>,
-    b: Point3<f32>,
-    c: Point3<f32>,
+    a: Point<f32>,
+    b: Point<f32>,
+    c: Point<f32>,
 }
 impl Triangle {
-    fn has_point(&self, pt: Point3<f32>) -> bool {
-        fn sign(a: Point3<f32>, b: Point3<f32>, c: Point3<f32>) -> f32 {
+    fn has_point(&self, pt: Point<f32>) -> bool {
+        fn sign(a: Point<f32>, b: Point<f32>, c: Point<f32>) -> f32 {
             (a.x - c.x) * (b.y - c.y) - (b.x - c.x) * (a.y - c.y)
         }
         let d1 = sign(pt, self.a, self.b);
@@ -20,10 +25,9 @@ impl Triangle {
         let mut coords_y = [self.a.y, self.b.y, self.c.y];
         coords_x.sort_by(|i, j| i.partial_cmp(j).unwrap());
         coords_y.sort_by(|i, j| i.partial_cmp(j).unwrap());
-        //min_x, min_y, max_x, max_y
         [coords_x[0], coords_y[0], coords_x[2], coords_y[2]]
     }
-    fn cartesian_to_barycentric(&self, pt: Point3<f32>) -> Point3<f32> {
+    fn cartesian_to_barycentric(&self, pt: Point<f32>) -> Point<f32> {
         let v0 = self.b - self.a;
         let v1 = self.c - self.a;
         let v2 = pt - self.a;
@@ -31,12 +35,12 @@ impl Triangle {
         let v = (v2.x * v1.y - v1.x * v2.y) * den;
         let w = (v0.x * v2.y - v2.x * v0.y) * den;
         let u = 1.0 - v - w;
-        Point3::new(u, v, w)
+        Point{x=u, y=v, z=w}
     }
-    fn barycentric_to_cartesian(&self, pt: Point3<f32>) -> Point3<f32> {
+    fn barycentric_to_cartesian(&self, pt: Point<f32>) -> Point<f32> {
         let x = pt.x * self.a.x + pt.y * self.b.x + pt.z * self.c.x;
         let y = pt.x * self.a.y + pt.y * self.b.y + pt.z * self.c.y;
         let z = pt.x * self.a.z + pt.y * self.b.z + pt.z * self.c.z;
-        Point3::new(x, y, z)
+        Point{x=x, y=y, z=z}
     }
 }
